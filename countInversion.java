4b6fe400ba950742b1232dfb62e147b664268eff
anuradha008 <anuradha08vidhale@gmail.com>
2023-07-01 09:42:29 +0530

    updated files

--- countInversion.java
+++ countInversion.java
@@ -3,6 +3,7 @@
 // arr[i]>arr[j];
 // j<i
 
+import java.util.ArrayList;
 
 public class countInversion {
      public static long getInversions(long arr[]) {
@@ -17,12 +18,54 @@ public class countInversion {
         }
         return count;
     }
+    public static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        int left = low;     
+        int right = mid + 1;  
+        int count = 0;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                count += (mid - left + 1);
+                right++;
+            }
+        }
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+        return count;
+    }
+    public static int mergeSortApprorch(int arr[],int low,int high){
+        int count = 0;
+        if (low >= high) return count;
+        int mid = (low + high) / 2 ;
+        count += mergeSortApprorch(arr, low, mid);  
+        count += mergeSortApprorch(arr, mid + 1, high); 
+        count += merge(arr, low, mid, high);
+        return count;
+    }
     public static void main(String[] args) {
-        long arr1[] ={ 10,52244275, 123047899 ,493394237, 922363607, 378906890 ,188674257 ,222477309 ,902683641 ,860884025 ,339100162,};
-        long arr2[]={3,2,1 };
-        long arr3[]= { 2,5,1,3,4};
-        System.out.println("The Number of pairs are : "+getInversions(arr1));
-        System.out.println("The Number of pairs are : "+getInversions(arr2));
-        System.out.println("The Number of pairs are : "+getInversions(arr3));
+        // long arr1[] ={ 10,52244275, 123047899 ,493394237, 922363607, 378906890 ,188674257 ,222477309 ,902683641 ,860884025 ,339100162,};
+        // long arr2[]={3,2,1 };
+        // long arr3[]= { 2,5,1,3,4};
+        // System.out.println("The Number of pairs are : "+getInversions(arr1));
+        // System.out.println("The Number of pairs are : "+getInversions(arr2));
+        // System.out.println("The Number of pairs are : "+getInversions(arr3));
+
+        int[] a = {5, 4, 3, 2, 1};
+        int n = 5;
+        int count = mergeSortApprorch(a, 0, n-1);
+        System.out.println("The number of inversions are: " + count);
     }
 }
--- sortArray.java
+++ sortArray.java
@@ -1,5 +1,72 @@
+/*
+ * Sort an array containg all 0,1,2 only
+ * Here we have 2 Approch
+ * 1.count0,count1,count2 variable : time complexity O(2n)
+ * 2.DNF algorithm 
+ */
+
 public class sortArray {
+    public static void betterSolution(int arr[]){
+        int count0 =0;
+        int count1= 0;
+        int count2 = 0;
+        for(int i =0 ; i<arr.length ;i++){
+            if(arr[i]==0){
+                 count0++;          
+            }else if(arr[i]==1){
+                 count1++;
+            }else{
+                count2++;
+             }
+       }  
+       System.out.println(count0);
+       System.out.println(count1);
+       System.out.println(count2); 
+                 
+                
+                
+        for(int i =0 ; i<count0 ; i++){
+            arr[i] = 0;
+        }
+        for(int i = count0 ; i<count0+count1 ; i++){
+            arr[i]=1;
+        }
+        for(int i = count0+count1 ; i<arr.length ; i++){
+            arr[i]=2;
+        }
+    }
+    public static void DNFAlgo(int arr[]){
+        int low = 0;
+        int mid =0 ;
+        int high =arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+
+                low++;
+                mid++;
+            }
+            else if(arr[mid] ==1){
+                mid++;
+            }
+            else{
+                int temp = arr[high];
+                arr[high] = arr[mid];
+                arr[mid] = temp;
+                high--;
+            }
+        }
+    }
     public static void main(String args[]){
+        int arr[]={0,1,2,0,1,2,1,2,0,0,0,1};
+        //betterSolution(arr);
+        DNFAlgo(arr);
+        for(int i =0 ; i<arr.length ; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
         
     }
     
